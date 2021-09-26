bbb7d11cfe309d109c1b4a601eaff6fd6084d1df
Arseniy Zinchenko <70096155+ArsenZ1nch@users.noreply.github.com>
2021-09-26 09:14:54 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,30 @@
+import discord
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('Ready')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('!start'):
+        await message.channel.send('k')
+        await message.channel.send('k')
+        await message.channel.send('k')
+        await message.channel.send('k')
+        await message.channel.send('k')
+        # arc = await client.fetch_user(539766493899128834)
+        arc = await client.fetch_user(529271392932331540)
+        while True:
+            for _ in range(5):
+                await arc.send('MONKE DOESNT HAVE **AMOGUS SYNDROME**')
+            for _ in range(5):
+                await arc.send('**YOU ARE SUS**')
+            for _ in range(5):
+                await arc.send('🙀 🙀 🙀 🙀 🙀')
+
+client.run('ODkxNTY1Mjc5ODUzODEzNzkw.YVAM3w.F-KDL63VGbZDilMDHIMGYWEoBus')
