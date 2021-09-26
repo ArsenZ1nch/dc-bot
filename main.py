3493cff54ec7537f5c245d0b9928ca398d5d68dd
Arseniy Zinchenko <70096155+ArsenZ1nch@users.noreply.github.com>
2021-09-26 10:58:30 +0200

    Update main.py

--- main.py
+++ main.py
@@ -15,17 +15,14 @@ async def on_message(message):
 
     if message.content.startswith('!start'):
         await message.channel.send('k')
-        arc0 = await client.fetch_user(710183974378536981)
+        # arc = await client.fetch_user(710183974378536981)
         arc = await client.fetch_user(809467185764106272)
         while True:
             for _ in range(5):
                 await arc.send('https://tenor.com/view/spam-gif-18321446')
-                await arc0.send('https://tenor.com/view/spam-gif-18321446')
             for _ in range(5):
                 await arc.send('https://tenor.com/view/spam-gif-18321446')
-                await arc0.send('https://tenor.com/view/no-spamming-gif-18157875')
             for _ in range(5):
-                await arc0.send('https://tenor.com/view/spam-gif-18321446')
                 await arc.send('https://c.tenor.com/11DOBMQ6FcUAAAAM/no-spamming.gif')
 
 client.run('ODkxNTY1Mjc5ODUzODEzNzkw.YVAM3w.F-KDL63VGbZDilMDHIMGYWEoBus')
